49c9391449de3554b1ffc67d97a3d51e285d63f5
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-11-09 00:31:16 +0000

    update

--- factorial_program.py
+++ factorial_program.py
@@ -2,36 +2,34 @@
 
 # Created by: Joanne Santhosh
 # Created on: Nov 7, 2022
+# This program uses a while loop
 
 
 def main():
+    # this function finds factorial of a number using while loop
 
-    loop_counter = 0
-    multiply_int = 1
-
-    # This function does the factorial calculation
-
-    # Input
-    user_input = input("Enter any positive integer:")
+    # input
+    int_as_string = input("Enter a positive integer: ")
     print("")
 
-    # Process and output
+    # variables
+    factorial = 1
+    loop_counter = 0
+
+    # process & output
     try:
-        integer = int(user_input)
+        integer = int(int_as_string)
         if integer >= 0:
             while loop_counter < integer:
                 loop_counter = loop_counter + 1
-                multiply_int = multiply_int * loop_counter
-                text = text + " x " + str(loop_counter)
-            print("\n{0} = {1}".format(integer, multiply_int))
-        elif integer == 0:
-            print("\n0 = 0")
+                factorial = factorial * loop_counter
+            print("{0}! = {1}.".format(integer, factorial))
         else:
-            print("\nError: {} is a negative integer.".format(user_input))
+            print("{0}is not a positive integer.".format(integer))
     except ValueError:
-        print("\nError: {} is not an integer.".format(user_input))
+        print("{0} is not an integer".format(int_as_string))
     finally:
-        print("\nDone.")
+        print("Done.")
 
 
 if __name__ == "__main__":
